60896c7ffbff0578679d1b14886525b82791043b
SakinahRahmahwati <sakinahrahmawati08@gmail.com>
2023-03-19 15:23:41 +0700

    update

--- src/Sakinah080323/GetInputFromKeyboards.java
+++ src/Sakinah080323/GetInputFromKeyboards.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Sakinah080323;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-/**
- *
- * @author HP-PC
- */
 public class GetInputFromKeyboards {
-    
+    public static void main(String[] args){
+        BufferedReader dataIn =
+                new BufferedReader(new InputStreamReader( System.in) );
+        String name = "";
+        System.out.print("Please Enter Your Name:");
+        try{
+            name = dataIn.readLine();
+        }catch( IOException e ){
+            System.out.println("Error!");
+        }
+        System.out.println("Hello " + name +"!");
+    }
 }
