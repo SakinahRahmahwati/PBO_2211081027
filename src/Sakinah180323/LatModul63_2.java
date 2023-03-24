bc5567b7de52415176c4cd299da1090cc4e01656
SakinahRahmahwati <sakinahrahmawati08@gmail.com>
2023-03-24 18:32:24 +0700

    update

---
+++ src/Sakinah180323/LatModul63_2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sakinah180323;
+public class LatModul63_2 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i + ". Sakinah Rahmahwati");
+            i++;
+        } while (i <= 100);
+    }
+}
+
---
+++ src/Sakinah180323/LatModul63_3.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sakinah180323;
+public class LatModul63_3 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            for (int j = 0; j < 1; j++) {
+                System.out.println(i + ". Sakinah Rahmahwati");
+            }
+            i++;
+        }
+    }
+}
+
+
